b608e91e8a5dc519f763815bae5178abb39669e2
Sanctuary <sanc.pw@gmail.com>
2019-02-16 22:48:19 -0500

    Add http client file

---
+++ src/http.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const { token } = JSON.parse(localStorage.getItem('react-user')) || {};
+const http = axios.create({ baseURL: 'http://localhost:4242' });
+
+http.setAuthHeader = function setAuthHeader(authToken) {
+	if (authToken) {
+		return this.defaults.headers.common.Authorization = `Bearer ${authToken}`;
+	}
+
+	delete this.defaults.headers.common.Authorization;
+};
+
+if (token) http.setAuthHeader(token);
+
+export default http;
